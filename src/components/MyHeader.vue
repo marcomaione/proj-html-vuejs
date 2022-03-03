cb63d0a24fff4f03498b7db762322d9f6985e94f
marco maione <maionemarco14@gmail.com>
2022-03-03 16:46:44 +0100

    aggiornamento

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="container">
+        <div class="row">
+          <div class="logo">
+              <img src="../assets/avada-img/logo.png" alt="">
+          </div>
+          <div class="nav">
+              <ul>
+                  <li>Home</li>
+                  <li>Service</li>
+                  <li>About</li>
+                  <li>Videos</li>
+                  <li>Blog</li>
+                  <li>Store</li>
+                  <button>schedule a workout</button>
+              </ul>
+
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"MyHeader"
+
+}
+</script>
+
+<style lang="scss">
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.container {
+    width: 100%;
+    min-height: 80px;
+    background-color:black;
+    .row {
+        width: 80%;
+        height: 80px;
+        margin: auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        .nav li {
+            display: inline-block;
+            margin:0px 7px;
+            color: #71888c;
+            
+        }
+        button {
+                background: #4154ff;
+                color: azure;
+                width: 150px;
+                height: 40px;
+                border-radius: 50px;
+                border: solid 1px #4154ff;
+            }
+    }
+}
+
+
+</style>
