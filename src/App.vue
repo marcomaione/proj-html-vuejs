2c16c8f043a87c339194a512f56800ec3c67b44a
marco maione <maionemarco14@gmail.com>
2022-03-04 23:30:36 +0100

    aggiornamento

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <MyHeader></MyHeader>
     <MyMain></MyMain>
+    <MyFooter></MyFooter>
   </div>
 </template>
 
 <script>
 import MyHeader from './components/MyHeader.vue'
 import MyMain from './components/MyMain.vue'
+import MyFooter from './components/MyFooter.vue'
 export default {
   name: 'App',
   components: {
     MyHeader,
     MyMain,
+    MyFooter,
 
   }
 }
---
+++ src/components/MyFooter.vue
@@ -0,0 +1,50 @@
+<template>
+    <section>
+            <div class="row-3">
+                <div class="sponsor">
+                    <img src="../assets/avada-img/sponsor-1-2x-200x103.png" alt="">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam.</p>
+                    <a href="#">Visit Yoga Studio ></a>
+                </div>
+                <div class="sponsor">
+                    <img src="../assets/avada-img/sponsor-2-2x-200x103.png" alt="">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam.</p>
+                    <a href="#">Join Fitness Center ></a>
+                </div>
+                <div class="sponsor">
+                    <img src="../assets/avada-img/sponsor-3-2x.png" alt="">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam.</p>
+                    <a href="#">Learn about FC ></a>
+                </div>
+                <div class="sponsor">
+                    <img src="../assets/avada-img/sponsor-4-2x-200x103.png" alt="">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quibusdam.</p>
+                    <a href="#">Visit Power Gym ></a>
+                </div>
+            </div>
+        </section>
+</template>
+
+<script>
+export default {
+    name:"MyFooter"
+
+}
+</script>
+
+<style lang="scss">
+.sponsor {
+    margin-top: 50px;
+    width: 20%;
+    text-align: center;
+    img {
+        max-width: 150px;
+        margin-bottom: 25px;
+    }
+    p {
+        margin-bottom: 25px;
+    }
+
+}
+
+</style>
